096d2691be99686942ff3a44ddd27b4d0dc67608
Tanya <tanya@example.com>
2023-01-25 20:49:47 +0300

    решение задачи 52

---
+++ homework/seminar7_(22.01.23)/task52/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 52: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+
+
+
+
+// Console.Clear();
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(-10,10);
+        }
+    }
+}
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArrayMidl(int[,] matr)
+{
+    Console.Write("Среднее значение элементов столбца: ");
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            sum += matr[j,i];
+        }
+        Console.Write($"{sum/matr.GetLength(0)}, ");
+    }
+}
+
+
+int [,] matrix = new int[4,4];
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+FillArrayMidl(matrix);
